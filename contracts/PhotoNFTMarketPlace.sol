ac906a86e7ebff4d6eced1f325945ddc2f3e2838
立青木白 <cywhoyi@gmail.com>
2021-12-20 20:47:26 +0800

    Update PhotoNFTMarketPlace.sol

--- contracts/PhotoNFTMarketPlace.sol
+++ contracts/PhotoNFTMarketPlace.sol
@@ -1,92 +1,155 @@
-pragma solidity ^0.5.16;
-pragma experimental ABIEncoderV2;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-//import { ERC20 } from './openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
-import { SafeMath } from "./openzeppelin-solidity/contracts/math/SafeMath.sol";
-import { PhotoNFT } from "./PhotoNFT.sol";
-import { PhotoNFTTradable } from "./PhotoNFTTradable.sol";
-import { PhotoNFTMarketplaceEvents } from "./photo-nft-marketplace/commons/PhotoNFTMarketplaceEvents.sol";
-import { PhotoNFTData } from "./PhotoNFTData.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract NFTCollection is ERC721, ERC721Enumerable {
+  string[] public tokenURIs;
+  mapping(string => bool) _tokenURIExists;
+  mapping(uint => string) _tokenIdToTokenURI;
+
+  constructor() 
+    ERC721("WOWARRIORS Collection", "WOWNFT") 
+  {
+  }
+
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
+
+  function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+    return super.supportsInterface(interfaceId);
+  }
+
+  function tokenURI(uint256 tokenId) public override view returns (string memory) {
+    require(_exists(tokenId), 'ERC721Metadata: URI query for nonexistent token');
+    return _tokenIdToTokenURI[tokenId];
+  }
+
+  function safeMint(string memory _tokenURI) public {
+    require(!_tokenURIExists[_tokenURI], 'The token URI should be unique');
+    tokenURIs.push(_tokenURI);    
+    uint _id = tokenURIs.length;
+    _tokenIdToTokenURI[_id] = _tokenURI;
+    _safeMint(msg.sender, _id);
+    _tokenURIExists[_tokenURI] = true;
+  }
+}
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract NFTMarketplace is AccessControl{
+  uint public offerCount;
+  mapping (uint => _Offer) public offers;
+  mapping (address => uint) public userFunds;
+  uint [] array;
+  NFTCollection nftCollection;
+  bool private pause = false;
+  bytes32 public constant MAKE_ROLE = keccak256("MAKE_OFFER_ROLE");
+  bytes32 public constant FILL_ROLE = keccak256("FILL_OFFER_ROLE");
+
+  struct _Offer {
+    uint offerId;
+    uint id;
+    address user;
+    uint price;
+    bool fulfilled;
+    bool cancelled;
+  }
+
+  event Offer(
+    uint offerId,
+    uint id,
+    address user,
+    uint price,
+    bool fulfilled,
+    bool cancelled
+  );
 
 
-contract PhotoNFTMarketplace is PhotoNFTTradable, PhotoNFTMarketplaceEvents {
-    using SafeMath for uint256;
+  event ClaimNFT(uint offerId, uint id, address newOwner);
+  event OfferFilled(uint offerId, uint id, address newOwner);
+  event OfferCancelled(uint offerId, uint id, address owner);
+  event ClaimFunds(address user, uint amount);
 
-    address public PHOTO_NFT_MARKETPLACE;
+  constructor(address _nftCollection) {
+    nftCollection = NFTCollection(_nftCollection);
+    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+  }
 
-    PhotoNFTData public photoNFTData;
-
-    constructor(PhotoNFTData _photoNFTData) public PhotoNFTTradable(_photoNFTData) {
-        photoNFTData = _photoNFTData;
-        address payable PHOTO_NFT_MARKETPLACE = address(uint160(address(this)));
+   modifier processApprove() {
+        require(!pause, "Contract paused");
+        _;
     }
-
-    /** 
-     * @notice - Buy function is that buy NFT token and ownership transfer. (Reference from IERC721.sol)
-     * @notice - msg.sender buy NFT with ETH (msg.value)
-     * @notice - PhotoID is always 1. Because each photoNFT is unique.
-     */
-    function buyPhotoNFT(PhotoNFT _photoNFT) public payable returns (bool) {
-        PhotoNFT photoNFT = _photoNFT;
-
-        PhotoNFTData.Photo memory photo = photoNFTData.getPhotoByNFTAddress(photoNFT);
-        address _seller = photo.ownerAddress;                     /// Owner
-        address payable seller = address(uint160(_seller));  /// Convert owner address with payable
-        uint buyAmount = photo.photoPrice;
-        require (msg.value == buyAmount, "msg.value should be equal to the buyAmount");
- 
-        /// Bought-amount is transferred into a seller wallet
-        seller.transfer(buyAmount);
-
-        /// Approve a buyer address as a receiver before NFT's transferFrom method is executed
-        address buyer = msg.sender;
-        uint photoId = 1;  /// [Note]: PhotoID is always 1. Because each photoNFT is unique.
-        photoNFT.approve(buyer, photoId);
-
-        address ownerBeforeOwnershipTransferred = photoNFT.ownerOf(photoId);
-
-        /// Transfer Ownership of the PhotoNFT from a seller to a buyer
-        transferOwnershipOfPhotoNFT(photoNFT, photoId, buyer);    
-        photoNFTData.updateOwnerOfPhotoNFT(photoNFT, buyer);
-        photoNFTData.updateStatus(photoNFT, "Cancelled");
-
-        /// Event for checking result of transferring ownership of a photoNFT
-        address ownerAfterOwnershipTransferred = photoNFT.ownerOf(photoId);
-        emit PhotoNFTOwnershipChanged(photoNFT, photoId, ownerBeforeOwnershipTransferred, ownerAfterOwnershipTransferred);
-
-        /// Mint a photo with a new photoId
-        //string memory tokenURI = photoNFTFactory.getTokenURI(photoData.ipfsHashOfPhoto);  /// [Note]: IPFS hash + URL
-        //photoNFT.mint(msg.sender, tokenURI);
-    }
-
-
-    ///-----------------------------------------------------
-    /// Methods below are pending methods
-    ///-----------------------------------------------------
-
-    /** 
-     * @dev reputation function is that gives reputation to a user who has ownership of being posted photo.
-     * @dev Each user has reputation data in struct
-     */
-    function reputation(address from, address to, uint256 photoId) public returns (uint256, uint256) {
-
-        // Photo storage photo = photos[photoId];
-        // photo.reputation = photo.reputation.add(1);
-
-        // emit AddReputation(photoId, photo.reputation);
-
-        // return (photoId, photo.reputation);
-        return (0, 0);
+  
+    
+    function setPause() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        pause = true;
     }
     
+    function unSetPause() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        pause = false;
+    }
 
-    function getReputationCount(uint256 photoId) public view returns (uint256) {
-        uint256 curretReputationCount;
 
-        // Photo memory photo = photos[photoId];
-        // curretReputationCount = photo.reputation;
+  function claimNFT(uint _offerId) public onlyRole(DEFAULT_ADMIN_ROLE){
+      _Offer storage _offer = offers[_offerId];
+      nftCollection.transferFrom(address(this), msg.sender, _offer.id);
+      emit ClaimNFT(_offerId, _offer.id, msg.sender);
+  }
+  
+  function addItem(uint _type, uint _id, uint _price) public onlyRole(MAKE_ROLE) processApprove{ 
+    nftCollection.transferFrom(msg.sender, address(this), _id);
+    offerCount ++;
+    offers[offerCount] = _Offer(offerCount, _id, msg.sender, _price, false, false);
+    emit Offer(offerCount, _id, msg.sender, _price, false, false);
+  }
 
-        return curretReputationCount;
-    }    
+  function buyItems(uint num) public payable onlyRole(FILL_ROLE) processApprove{ 
+      require(offerCount - num > 0, 'The offer must exist');
+      for(uint i=1;i<=num;i++){
+        require(offerCount > 0, 'The offer must exist');
+        _Offer storage _offer = offers[offerCount];
+        require(msg.value >= _offer.price * num, 'The ETH amount should match with the NFT Price');
+        require(_offer.user != msg.sender, 'The owner of the offer cannot fill it');
+        require(!_offer.fulfilled, 'An offer cannot be fulfilled twice');
+        require(!_offer.cancelled, 'A cancelled offer cannot be fulfilled');
+        require(msg.value == _offer.price, 'The ETH amount should match with the NFT Price');
+        nftCollection.transferFrom(address(this), msg.sender, _offer.id);
+        _offer.fulfilled = true;
+        userFunds[_offer.user] += msg.value;
+        offerCount--;
+        emit OfferFilled(_offer.id, _offer.id, msg.sender);
+      }
+  }
 
+  function buyItem() public payable onlyRole(FILL_ROLE) processApprove{ 
+    require(offerCount > 0, 'The offer must exist');
+    _Offer storage _offer = offers[offerCount];
+    require(_offer.user != msg.sender, 'The owner of the offer cannot fill it');
+    require(!_offer.fulfilled, 'An offer cannot be fulfilled twice');
+    require(!_offer.cancelled, 'A cancelled offer cannot be fulfilled');
+    require(msg.value == _offer.price, 'The ETH amount should match with the NFT Price');
+    nftCollection.transferFrom(address(this), msg.sender, _offer.id);
+    _offer.fulfilled = true;
+    userFunds[_offer.user] += msg.value;
+    offerCount--;
+    emit OfferFilled(_offer.id, _offer.id, msg.sender);
+  }
+
+  function claimFunds() public {
+    require(userFunds[msg.sender] > 0, 'This user has no funds to be claimed');
+    payable(msg.sender).transfer(userFunds[msg.sender]);
+    emit ClaimFunds(msg.sender, userFunds[msg.sender]);
+    userFunds[msg.sender] = 0;    
+  }
+
+  // Fallback: reverts if Ether is sent to this smart-contract by mistake
+  fallback () external {
+    revert();
+  }
 }
